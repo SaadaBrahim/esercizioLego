7cac899b6847f892eb6a432382a408d0b7ef7967
SaadaBrahim <saada.brahimbryan.studente@itispaleocapa.it>
2025-10-31 12:42:07 +0100

    Add constructor and methods to manage Lego piecesmetod
    Implemented a constructor and methods for managing Lego pieces in the Prodotto class.

--- Prodotto.java
+++ Prodotto.java
@@ -1,2 +1,64 @@
+import java.util.Arrays;
+
 public class Prodotto {
+  private String codiceNumerico;
+  private String denominazione;
+  private int nLegoTotali;
+  private Lego[] listaMattoncini;
+
+  //metodo cost
+      public Prodotto(String codiceNumerico, String denominazione) {
+        this.codiceNumerico = codiceNumerico;
+        this.denominazione = denominazione;
+        this.listaMattoncini = new Lego[10]; // 10 
+        this.nLegoTotali = 0;
+    }
+
+  public void aggiungiMattoncino(Lego nuovoLego) {
+        if (nLegoTotali == listaMattoncini.length) {
+            raddoppiaSpazio(); // se pieno rad spazio
+        }
+        listaMattoncini[nLegoTotali] = nuovoLego;
+        nLegoTotali++;
+  }
+
+private void raddoppiaSpazio() {
+    listaMattoncini = Arrays.copyOf(listaMattoncini, listaMattoncini.length * 2);
+    System.out.println("spazio raddoppiato a " + listaMattoncini.length + " elementi.");
+}
+  
+  public String getCodiceNumerico() {
+        return codiceNumerico;
+    }
+
+    public void setCodiceNumerico(String codiceNumerico) {
+        this.codiceNumerico = codiceNumerico;
+    }
+
+
+    public String getDenominazione() {
+        return denominazione;
+    }
+
+    public void setDenominazione(String denominazione) {
+        this.denominazione = denominazione;
+    }
+
+
+    public int getNLegoTotali() {
+        return nLegoTotali;
+    }
+
+    public void setNLegoTotali(int nLegoTotali) {
+        this.nLegoTotali = nLegoTotali;
+    }
+
+    public Lego[] getListaMattoncini() {
+        return listaMattoncini;
+    }
+
+    public void setListaMattoncini(Lego[] listaMattoncini) {
+        this.listaMattoncini = listaMattoncini;
+    }
+  
 }
