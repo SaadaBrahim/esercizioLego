c9d1f050c7891b47c1f6e32c6e69a5c96953d82c
SaadaBrahim <saada.brahimbryan.studente@itispaleocapa.it>
2025-10-31 12:26:59 +0100

    Rgb creazione get/set e mettodi cost.
    Added fields and methods for RGB color representation.

--- Rgb.java
+++ Rgb.java
@@ -1,2 +1,38 @@
 public class Rgb {
+  private int red;
+  private int green;
+  private int blue;
+
+public Rgb(){}
+
+public Rgb(int red, int green, int blue){
+  this.red = red;
+  this.green = green;
+  this.blue = blue;
+
+public int getRed() {
+        return red;
+    }
+
+    public void setRed(int red) {
+        this.red = red;
+    }
+
+    // Getter e Setter per green
+    public int getGreen() {
+        return green;
+    }
+
+    public void setGreen(int green) {
+        this.green = green;
+    }
+
+    // Getter e Setter per blue
+    public int getBlue() {
+        return blue;
+    }
+
+    public void setBlue(int blue) {
+        this.blue = blue;
+    }
 }
